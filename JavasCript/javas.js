dacb6c77ecfcab701ab695018029ce3f5dbabd77
mwsabadini <120953258+mwsabadini@users.noreply.github.com>
2023-05-16 08:33:11 -0300

    scripts para el formulario

--- JavasCript/javas.js
+++ JavasCript/javas.js
@@ -1,1 +1,27 @@
 
+
+        
+
+function validarformulario(){
+    
+    const msg=document.getElementById("mensaje");
+    const nombre = document.getElementById("name").value;
+    const ndoc=document.getElementsByName("dni")[0].value;
+    const apel = document.getElementById("lastname").value;
+
+    if (ndoc < 10 || ndoc == null) {
+        
+        msg.innerHTML="Verifique el DNI ingresado";
+
+    } else if(nombre.length < 2) {
+        
+        msg.innerHTML="Verifique el nombre ingresado";
+    } else  {
+        msg.innerHTML="Bienvenid@ " + nombre + " " + apel;
+
+    }
+      
+    
+
+}
+
